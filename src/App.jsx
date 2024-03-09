208991b10039e96ce997c8966fa72d992326f699
Abraham Agusti <abraham.agusti@gmail.com>
2024-03-09 16:47:15 +0100

    iteration 0 and 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import "./App.css";
+import "./companies.json"
+import "./technologies.json"
+import { useState } from "react";
+
+
+
+
 
 function App() {
+
+  const [companies,setCompanies] = useState(companies.json)
+  const [technologies,setTechnologies] = useState(technologies.json)
+
+
   return (
     <div className="App">
+      
       <h1>LAB | React Stack Tracker</h1>
     </div>
   );
